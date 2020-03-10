6790bc47ad9008d8aef93ebf6faf0adbb98dec2c
jdarko <jevtic.darko.3@gmail.com>
2020-03-10 15:35:19 +0100

    creation de la struct et des define

--- includes/ft_malloc.h
+++ includes/ft_malloc.h
@@ -1,4 +1,26 @@
-#include <unistd.h>
-#include <string.h>
+
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
+
+# include <unistd.h>
+# include <string.h>
+
+# define ts             112
+# define ss             1536
+# define structSize     16
+
+typedef struct          s_allocInfo
+{
+    struct allocInfo    *next;
+    size_t              size : 7;
+    int                 isFree : 1;
+}                       allocInfo;
+
+typedef struct          s_pages
+{
+    struct allocInfo    *tiny;
+    struct allocInfo    *small;
+    struct allocInfo    *large;
+}                       pages;
 
 int myfunct(char *str);
